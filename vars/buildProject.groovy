5191131b5f1c580cb9d8b4cc4a90ddb2f5dad6fb
Daniel Doubleday <daniel.doubleday@wooga.net>
2019-06-25 19:56:58 +0200

    Add a first pipeline

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -2,4 +2,20 @@
 
 def call(Map config = [name: "Not set"]) {
   println("Build called with ${config.name}!")
+
+  pipeline {
+    agent any
+
+    environment {
+      SOME_ENV = 'some environment'
+    }
+
+    stages {
+      stage('Build') {
+        steps {
+            sh 'printenv'
+        }
+      }
+    }
+  }
 }
